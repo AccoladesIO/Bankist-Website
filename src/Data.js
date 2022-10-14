dc13de211639276f815793442a9476f071f8cbf0
unknown <ononobip@gmail.com>
2022-10-14 22:49:13 +0100

    data

---
+++ src/Data.js
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        id: 1,
+        text: 'Home',
+        url: '#home'
+    },
+    {
+        id: 2,
+        text: 'Feautures',
+        url: '#feautures'
+    },
+    {
+        id: 3,
+        text: 'Operations',
+        url: '#operations'
+    },
+    {
+        id: 4,
+        text: 'Testimonials',
+        url: '#testimonial'
+    }
+]
